42284947ea92befeff759271d0d90dbd9352bb02
Steven Kirk <grokys@users.noreply.github.com>
2024-01-04 16:27:45 +0100

    WIP

---
+++ build.ps1
@@ -0,0 +1,14 @@
+$version=11.0.6
+$packages=@('Avalonia', 'Avalonia.Desktop')
+
+mkdir -Force packages
+
+foreach ($package in $packages)
+{
+	$dest="packages\$package"
+	if (!(Test-Path($dest)))
+	{
+		Invoke-WebRequest https://www.nuget.org/api/v2/package/$package/$version -OutFile packages/$package.zip
+		Expand-Archive -DestinationPath packages/$package packages/$package.zip
+	}
+}
